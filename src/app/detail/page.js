75c7000a6b8c3134463cc67bec641bab78253924
ymatsukawa <ymatsukawa27@gmail.com>
2023-09-01 19:59:42 +0900

    add pages

---
+++ src/app/detail/page.js
@@ -0,0 +1,7 @@
+export default function Detail() {
+  return (
+    <main>
+      <h1>Search</h1>
+    </main>
+  )
+}
---
+++ src/app/error/server_error/page.js
@@ -0,0 +1,7 @@
+export default function ServerError() {
+  return (
+    <main>
+      <h1>Server Error</h1>
+    </main>
+  )
+}
